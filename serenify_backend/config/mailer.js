61171164471a4b1467a396128e869468c5892602
Comscat-72 <igirimbabazie72@gmail.com>
2024-09-02 09:55:49 +0200

    New updated system

--- serenify_backend/config/mailer.js
+++ serenify_backend/config/mailer.js
@@ -4,6 +4,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   service: 'gmail',
+  secure: "true",
+  port: 465,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
